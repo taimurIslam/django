01db52a3b062e0e67d5157eeeabc241985255d5d
taimurIslam <taimur.joy@gmail.com>
2018-02-06 10:51:11 +0600

    3nd update

--- smart life/smartlife_4/website/music/views.py
+++ smart life/smartlife_4/website/music/views.py
@@ -7,15 +7,9 @@ from django.template import loader
 # Create your views here.
 def index(request):
     all_albums = Album.objects.all()
-    #template = loader.get_template('music/index.html')
-    # html = ''
-    # for album in all_albums:
-    #     url = '/music/'+str(album.id)+'/'
-    #     html += '<a href="'+url+'">'+album.album_title+'</a><br>'
-    context = {'all_albums':all_albums,}
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums':all_albums,})
 
-def view_details(request,album_id):
+def view_details(request, album_id):
     try:
         album = Album.objects.get(pk=album_id)
     except Album.DoesNotExist:
