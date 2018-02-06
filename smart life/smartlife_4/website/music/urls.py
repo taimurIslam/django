a3775274300eef69568ec622c8cac167a33cac78
taimurIslam <taimur.joy@gmail.com>
2018-02-06 16:02:40 +0600

    4th update

--- smart life/smartlife_4/website/music/urls.py
+++ smart life/smartlife_4/website/music/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, re_path
 from . import views
 
-
+#app_name = 'music'
 urlpatterns = [
     re_path('^$', views.index, name='index'),
-    re_path('(?P<album_id>[0-9]+)/', views.view_details, name='view_details')
+    re_path('(?P<album_id>[0-9]+)/$', views.view_details, name='view_details'),
+    re_path('(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    re_path('(?P<album_id>[0-9]+)/tausif/$', views.tausif, name='tausif'),
+
 ]
