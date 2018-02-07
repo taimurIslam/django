80baece3a8760fdd049785f81448872f81616d34
taimurIslam <taimur.joy@gmail.com>
2018-02-07 23:32:53 +0600

    update8

--- smart life/smartlife_5/main_website/user_registration/urls.py
+++ smart life/smartlife_5/main_website/user_registration/urls.py
@@ -4,8 +4,15 @@ from . import views
 urlpatterns = [
 
     re_path('^$', views.view_registration_page, name='view_registration_page'),
-    re_path('^new_user_registered/$', views.insert_register_form_values_in_database, name='insert_register_form_values_in_database'),
+    re_path('^new_user_registered/$', views.insert_register_form_values_in_database,
+            name='insert_register_form_values_in_database'),
     re_path('^registered_user_list/$', views.registered_user_list, name='registered_user_list'),
-    re_path('^registered_user_data_edit/(?P<registered_user_id>[0-9]+)/$', views.registered_user_data_edit, name='registered_user_data_edit'),
-    re_path('^registered_user_data_delete/(?P<registered_user_id>[0-9]+)/$', views.registered_user_data_delete, name='registered_user_data_delete'),
+    re_path('^registered_user_data_edit/(?P<registered_user_id>[0-9]+)/$',
+            views.registered_user_data_edit, name='registered_user_data_edit'),
+    re_path('^registered_user_data_delete/(?P<registered_user_id>[0-9]+)/$',
+            views.registered_user_data_delete, name='registered_user_data_delete'),
+
+    re_path('^registered_user_data_edited/(?P<registered_user_id>[0-9]+)/$',
+            views.registered_user_data_edited, name='registered_user_data_edited'),
+
 ]
--- smart life/smartlife_5/main_website/user_registration/views.py
+++ smart life/smartlife_5/main_website/user_registration/views.py
@@ -25,7 +25,7 @@ def insert_register_form_values_in_database(request):
 def registered_user_list(request):
     registered_user_list = Registration.objects.all()
     print(registered_user_list)
-    return render(request, 'registered_user_list.html', {'registered_user_list':registered_user_list,} )
+    return render(request, 'registered_user_list.html', {'registered_user_list': registered_user_list,} )
 
 
 def registered_user_data_edit(request, registered_user_id ):
@@ -33,12 +33,37 @@ def registered_user_data_edit(request, registered_user_id ):
     #return HttpResponse("edited")
     registered_user_edit_or_delete_all_information = get_object_or_404(Registration, pk = registered_user_id)
     print(registered_user_edit_or_delete_all_information.phone_number)
-    return render(request, 'registered_user_edit_or_delete.html', {'registered_user_edit_or_delete_all_information': registered_user_edit_or_delete_all_information, })
+    return render(request, 'registered_user_edit_or_delete.html',
+                  {'registered_user_edit_or_delete_all_information': registered_user_edit_or_delete_all_information, })
 
 
 def registered_user_data_delete(request, registered_user_id):
-    print(registered_user_id)
-    return HttpResponse("deleted")
+    registered_user_list = get_object_or_404(Registration, pk = registered_user_id)
+    registered_user_list.delete()
+    registered_user_list = Registration.objects.all()
+    return render(request, 'registered_user_list.html', {'registered_user_list': registered_user_list, })
+
+
+
+def registered_user_data_edited(request, registered_user_id):
+    if request.method == 'POST':
+        user_registration_informations = get_object_or_404(Registration, pk = registered_user_id)
+        user_registration_informations.first_name = request.POST['first_name']
+        user_registration_informations.last_name = request.POST['last_name']
+        user_registration_informations.phone_number = request.POST['phone_number']
+        user_registration_informations.email_address = request.POST['email']
+        user_registration_informations.user_name = request.POST['user_name']
+        user_registration_informations.user_password = request.POST['password']
+        user_registration_informations.user_address = request.POST['address']
+        user_registration_informations.save()
+
+        registered_user_list = Registration.objects.all()
+        return render(request, 'registered_user_list.html', {'registered_user_list': registered_user_list, })
+
+
+
+
+
 
 
 
